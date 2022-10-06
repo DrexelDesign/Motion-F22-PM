d798b833ff855c1c4c89d34ea33009c21426c61b
Josh Gdovin <jgg32@drexel.edu>
2022-10-06 13:32:21 -0400

    working demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,27 +1,40 @@
 import { gsap } from "gsap";
 gsap.set("#trails-btn i",{transformOrigin:"center bottom"});
 
-gsap.from("#first-line",{duration: 1, alpha:0, y:-100});
-gsap.from("#second-line",{duration: 1, alpha:0, y:-70, delay:0.5});
-gsap.from("#trails-btn",{duration: 1, alpha:0, y:50, delay:0.75});
-gsap.from("#trails-btn i",{duration: 1, alpha:0, rotation:180, delay:0.75});
+function heroAnimation(){
+    var tl = gsap.timeline();
+    tl.from("#first-line",{duration: 1, alpha:0, y:-100})
+        .from("#second-line",{duration: 1, alpha:0, y:-70})
+        .from("#trails-btn",{duration: 1, alpha:0, y:50})
+        .from("#trails-btn i",{duration: 1, alpha:0, rotation:180});
+    return tl;
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(heroAnimation());
+
+
+
+
 
 let trailBtn = document.querySelector("#trails-btn");
 
+var buttonAnimation = gsap.timeline();
+buttonAnimation.to("#trails-btn",{duration:1, scale:3});
+//         .to("#trails-btn i",{duration: 1, rotateY:180})
+//         .to("#first-line",{duration: 1, alpha:0, y:50})
+//         .to("#second-line",{duration: 1, alpha:0, y:20});
+
+
 trailBtn.addEventListener("mouseover",function(){
-    gsap.to("#trails-btn",{duration:1, scale:2});
-
-    gsap.to("#trails-btn i",{duration: 1, rotateY:180});
-
-    gsap.to("#first-line",{duration: 1, alpha:0, y:50});
-    gsap.to("#second-line",{duration: 1, alpha:0, y:20});
+   
 })
 
 trailBtn.addEventListener("mouseout",function(){
-    gsap.to("#trails-btn",{duration:1, scale:1});
+    // gsap.to("#trails-btn",{duration:1, scale:1});
 
-    gsap.to("#trails-btn i",{duration: 1, rotateY:0});
+    // gsap.to("#trails-btn i",{duration: 1, rotateY:0});
 
-    gsap.to("#first-line",{duration: 1, alpha:1, y:0});
-    gsap.to("#second-line",{duration: 1, alpha:1, y:0});
+    // gsap.to("#first-line",{duration: 1, alpha:1, y:0});
+    // gsap.to("#second-line",{duration: 1, alpha:1, y:0});
 })

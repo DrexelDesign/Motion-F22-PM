e4a4bc01942d3324acf0b867636c165d2ecf662e
Josh <joshGdovindesigns.com>
2022-10-13 08:06:05 -0400

    added demo files

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,45 +1,35 @@
+// console.log("hello class");
 import { gsap } from "gsap";
-gsap.set("#trails-btn i",{transformOrigin:"center bottom"});
+
+// Sets
+gsap.set("#hero h1 span",{alpha:0.25})
+
+var heroButtonTL = gsap.timeline({paused:true});
+    heroButtonTL.to("#trails-btn",{duration:0.25,scale:2, backgroundColor:"#881d02"},"trigger")  
+    .to("#first-line",{duration:0.25, alpha:0, y:50},"trigger")
+    .to("#second-line",{duration:0.25, alpha:0, y:20},"trigger")
+    .to("#trails-btn i",{duration:0.25, rotateY:180},"-=0.15");
+
+
+var trailsBtn = document.querySelector("#trails-btn");
+
+trailsBtn.addEventListener("mouseover",function(){
+    heroButtonTL.play();
+})
+
+trailsBtn.addEventListener("mouseout",function(){
+    heroButtonTL.reverse();
+})
 
 function heroAnimation(){
     var tl = gsap.timeline();
-    tl.from("#first-line",{duration: 1, alpha:0, y:-100})
-        .from("#second-line",{duration: 1, alpha:0, y:-70})
-        .from("#trails-btn",{duration: 1, alpha:0, y:50})
-        .from("#trails-btn i",{duration: 1, alpha:0, rotation:180});
+    tl.from("#first-line",{duration:1,alpha:0, y:-100})
+    .from("#second-line",{duration:1,alpha:0, y:-100},"-=0.75")
+    .from("#trails-btn",{duration:1,y:100, alpha:0},"-=.5")
+    .from("#trails-btn i",{duration:0.5,rotation:90, alpha:0, transformOrigin: "left bottom"},"-=0.5");
     return tl;
 }
 
-var mainTL = gsap.timeline();
-mainTL.add(heroAnimation());
 
-
-var heroSizeNumber = 1;
-
-let mm = gsap.matchMedia();
-
-mm.add("(min-width: 768px)", () => {
-  // desktop setup code here...
-  heroSizeNumber = 2;
-});
-
-mm.add("(max-width: 767px)", () => {
-  // mobile setup code here...
-  heroSizeNumber = 1.25;
-});
-
-let trailBtn = document.querySelector("#trails-btn");
-
-var buttonAnimation = gsap.timeline({paused:true});
-buttonAnimation.to("#trails-btn",{duration:0.25, scale:heroSizeNumber},"goAway")
-        .to("#trails-btn i",{duration: 0.25, rotateY:180})
-        .to("#first-line",{duration: 0.25, alpha:0, y:50},"goAway")
-        .to("#second-line",{duration: 0.25, alpha:0, y:20},"goAway");
-
-trailBtn.addEventListener("mouseover",function(){
-   buttonAnimation.play();
-})
-
-trailBtn.addEventListener("mouseout",function(){
-    buttonAnimation.reverse();
-})
+var mainTimeline = gsap.timeline();
+mainTimeline.add(heroAnimation());

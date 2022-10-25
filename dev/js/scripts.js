6b7ab00066a6d3a1ed39f2cd807360e228cfc7be
Josh <joshGdovindesigns.com>
2022-10-25 14:23:38 -0400

    draw svg demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,24 @@
 import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 
-gsap.to("#svgThing",{duration:1, fill:"red",x:300});
+function boxAnimation(){
+    var tl = gsap.timeline();
+    tl.fromTo("#box",{drawSVG:"0% 5%"},{duration:1, fill:"red", drawSVG:"95% 100%"})
+    .from("#line",{duration:1, drawSVG:"0%"});
+    return tl;
+}
+
+
+var mainTl = gsap.timeline();
+mainTl.add(boxAnimation());
+
+
+
+
+
+
+GSDevTools.create();

a4a85d2e24e75287464b16b53c47e381898b5988
Josh Gdovin <jgg32@drexel.edu>
2022-10-27 13:20:36 -0400

    added gsap

---
+++ dev/js/scripts.js
@@ -0,0 +1,10 @@
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+
+
+
+
+GSDevTools.create();

cdf4d9f655cdc40f5d2d36a2cdd94059635a062a
Josh Gdovin <jgg32@drexel.edu>
2022-10-27 13:23:52 -0400

    added js setup code

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,5 +6,30 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 
 
+function simpleMotion(){
+    var tl = gsap.timeline()
+
+    return tl;
+}
+
+function patternMotion(){
+    var tl = gsap.timeline()
+
+    return tl;
+}
+
+function UIMotion(){
+    var tl = gsap.timeline()
+
+    return tl;
+}
+
+
+var mainTL = gsap.timeline();
+mainTL.add(simpleMotion())
+    .add(patternMotion())
+    .add(UIMotion());
+
+
 
 GSDevTools.create();

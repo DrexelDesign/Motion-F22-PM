fb661d937df031ddd8372df3f40951bdb6ad5090
Josh Gdovin <jgg32@drexel.edu>
2022-10-27 13:56:32 -0400

    pattern complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -14,13 +14,21 @@ function simpleMotion(){
 
 function patternMotion(){
     var tl = gsap.timeline()
-
+    tl.from(".odd",{duration:0.5, rotate:180, drawSVG:0, stagger:0.25, transformOrigin:"center"},"playPattern")
+    .fromTo(".even",{drawSVG:"0% 0%"},{duration:0.5, rotate:180, drawSVG:"0% -100%", stagger:0.25, transformOrigin:"center"},"playPattern")
     return tl;
 }
 
 function UIMotion(){
-    var tl = gsap.timeline()
 
+    // make a var that represents the line in the SVG
+    var bottomLine = document.querySelector("#bottom-line");
+    // make a var that uses the svg line var from above and sets the BBox property
+    bottomLine = bottomLine.getBBox();
+
+    var tl = gsap.timeline()
+    tl.to("#pencil",{duration:0.5,x:bottomLine.width},"drawIn")
+    .from("#bottom-line",{duration:0.5,drawSVG:0},"drawIn")
     return tl;
 }
 

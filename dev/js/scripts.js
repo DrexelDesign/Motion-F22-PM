685493ac1e1e2f2e9784ae110e1a2e98c4c17fae
Josh <joshGdovindesigns.com>
2022-11-15 13:37:05 -0500

    demo complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,6 +3,8 @@ import { GSDevTools } from "gsap/GSDevTools";
 
 gsap.registerPlugin(GSDevTools);
 
+gsap.to("#shades",{duration:1,rotate:180, transformOrigin:'center'});
+gsap.to("#clip",{duration:1,scale:0, transformOrigin:'center',rotate:-180});
 
 GSDevTools.create();
 
